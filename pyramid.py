08a665cfcce067301840f1f24a79c86f26cd319f
bizzylizzy04 <bizzylizzy04@users.noreply.github.com>
2026-02-27 14:06:31 -0600

    update functions

--- pyramid.py
+++ pyramid.py
@@ -2,6 +2,7 @@ def pyramid(symbol, height):
     for i in range(1, height + 1):
         print(symbol * i)
 
-pyramid("*", 2)
-pyramid("+", 5)
-pyramid("x", 10)
+def main():
+    pyramid("*", 2)
+    pyramid("+", 5)
+    pyramid("x", 10)
